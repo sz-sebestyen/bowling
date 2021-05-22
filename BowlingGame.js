0940c2ea56b7c58a602960f0bf81e449a96836aa
Sebestyen Szabo <szabosebestyen.mail@gmail.com>
2021-05-22 12:59:48 +0200

    Validate the length of the input gamestring

--- BowlingGame.js
+++ BowlingGame.js
@@ -1,6 +1,25 @@
+const MAX_GAMESTRING_LENGTH = 32;
+const MIN_GAMESTRING_LENGTH = 20;
+
 module.exports = class BowlingGame {
   constructor(gameString) {
+    this.gameString = gameString;
+    this._frames = [];
+    this._bonusBalls = [];
+    this._score = 0;
+  }
 
+  static _parseFrames(gameString) {
+    const isTooLong = (str) => str.length > MAX_GAMESTRING_LENGTH;
+    const isTooShort = (str) => str.length < MIN_GAMESTRING_LENGTH;
+
+    if (isTooLong) {
+      throw RangeError("Gamestring is too long!");
+    } else if (isTooShort) {
+      throw RangeError("Gamestring is too short!");
+    }
+
+    return [];
   }
 
   getScore() {

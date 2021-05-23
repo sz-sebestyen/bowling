70b2a7e236df73f58dccd90082da3e3e1226f62b
Sebestyen Szabo <szabosebestyen.mail@gmail.com>
2021-05-23 09:50:14 +0200

    Add test from open frames

--- regex.test.js
+++ regex.test.js
@@ -19,3 +19,16 @@ it("Match a missed ball", () => {
     expect(missBallMatcher.test(`${i}`)).toBeFalsy();
   }
 });
+
+it("Match open frames", () => {
+  expect(openFrameMatcher.test("11")).toBeTruthy();
+  expect(openFrameMatcher.test("-9")).toBeTruthy();
+  expect(openFrameMatcher.test("5-")).toBeTruthy();
+  expect(openFrameMatcher.test("55")).toBeTruthy();
+  expect(openFrameMatcher.test("x")).toBeFalsy();
+  expect(openFrameMatcher.test("-/")).toBeFalsy();
+  expect(openFrameMatcher.test("9/")).toBeFalsy();
+  for (let i = 1; i < 9; i++) {
+    expect(openFrameMatcher.test(`${i}${9 - i}`)).toBeTruthy();
+  }
+});

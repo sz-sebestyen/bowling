2bef97ad5043eeb570e846fdd9ab29486f7ea063
Sebestyen Szabo <szabosebestyen.mail@gmail.com>
2021-05-23 10:19:11 +0200

    Orgenize test into describes and subtests

--- regex.test.js
+++ regex.test.js
@@ -28,56 +28,76 @@ describe("Match a missed ball", () => {
   });
 });
 
-it("Match open frames", () => {
-  expect(openFrameMatcher.test("11")).toBeTruthy();
-  expect(openFrameMatcher.test("-9")).toBeTruthy();
-  expect(openFrameMatcher.test("5-")).toBeTruthy();
-  expect(openFrameMatcher.test("55")).toBeTruthy();
-  expect(openFrameMatcher.test("x")).toBeFalsy();
-  expect(openFrameMatcher.test("-/")).toBeFalsy();
-  expect(openFrameMatcher.test("--")).toBeFalsy();
-  expect(openFrameMatcher.test("9/")).toBeFalsy();
-  expect(openFrameMatcher.test("111")).toBeFalsy();
-  for (let i = 1; i < 9; i++) {
-    expect(openFrameMatcher.test(`${i}${9 - i}`)).toBeTruthy();
-  }
+describe("Match open frames", () => {
+  it("Correct", () => {
+    expect(openFrameMatcher.test("11")).toBeTruthy();
+    expect(openFrameMatcher.test("-9")).toBeTruthy();
+    expect(openFrameMatcher.test("5-")).toBeTruthy();
+    expect(openFrameMatcher.test("55")).toBeTruthy();
+    for (let i = 1; i < 9; i++) {
+      expect(openFrameMatcher.test(`${i}${9 - i}`)).toBeTruthy();
+    }
+  });
+
+  it("Don't match strikes spares and strings longer than 2", () => {
+    expect(openFrameMatcher.test("x")).toBeFalsy();
+    expect(openFrameMatcher.test("-/")).toBeFalsy();
+    expect(openFrameMatcher.test("--")).toBeFalsy();
+    expect(openFrameMatcher.test("9/")).toBeFalsy();
+    expect(openFrameMatcher.test("111")).toBeFalsy();
+  });
 });
 
-it("Match spare frames", () => {
-  expect(spareFrameMatcher.test("x")).toBeFalsy();
-  expect(spareFrameMatcher.test("//")).toBeFalsy();
-  expect(spareFrameMatcher.test("-9")).toBeFalsy();
-  expect(spareFrameMatcher.test("-/")).toBeTruthy();
-  expect(spareFrameMatcher.test("-9/")).toBeFalsy();
-  for (let i = 1; i < 10; i++) {
-    expect(spareFrameMatcher.test(`${i}/`)).toBeTruthy();
-  }
+describe("Match spare frames", () => {
+  it("Correct", () => {
+    expect(spareFrameMatcher.test("-/")).toBeTruthy();
+    for (let i = 1; i < 10; i++) {
+      expect(spareFrameMatcher.test(`${i}/`)).toBeTruthy();
+    }
+  });
+
+  it("Don't match strikes, open frames and too long frames", () => {
+    expect(spareFrameMatcher.test("x")).toBeFalsy();
+    expect(spareFrameMatcher.test("//")).toBeFalsy();
+    expect(spareFrameMatcher.test("-9")).toBeFalsy();
+    expect(spareFrameMatcher.test("-9/")).toBeFalsy();
+  });
 });
 
-it("Match strike frames", () => {
-  expect(strikeFrameMatcher.test("x")).toBeTruthy();
-  expect(strikeFrameMatcher.test("X")).toBeTruthy();
-  expect(strikeFrameMatcher.test("xx")).toBeFalsy();
-  expect(strikeFrameMatcher.test("//")).toBeFalsy();
-  expect(strikeFrameMatcher.test("-/")).toBeFalsy();
-  for (let i = 1; i < 10; i++) {
-    expect(strikeFrameMatcher.test(`${i}/`)).toBeFalsy();
-  }
-  for (let i = 1; i < 9; i++) {
-    expect(strikeFrameMatcher.test(`${i}${9 - i}`)).toBeFalsy();
-  }
+describe("Match strike frames", () => {
+  it("Correct", () => {
+    expect(strikeFrameMatcher.test("x")).toBeTruthy();
+    expect(strikeFrameMatcher.test("X")).toBeTruthy();
+  });
+
+  it("Don't match too long frames, spares and open frams", () => {
+    expect(strikeFrameMatcher.test("xx")).toBeFalsy();
+    expect(strikeFrameMatcher.test("//")).toBeFalsy();
+    expect(strikeFrameMatcher.test("-/")).toBeFalsy();
+    for (let i = 1; i < 10; i++) {
+      expect(strikeFrameMatcher.test(`${i}/`)).toBeFalsy();
+    }
+    for (let i = 1; i < 9; i++) {
+      expect(strikeFrameMatcher.test(`${i}${9 - i}`)).toBeFalsy();
+    }
+  });
 });
 
-it("Match missed frames", () => {
-  expect(missFrameMatcher.test("-")).toBeTruthy();
-  expect(missFrameMatcher.test("//")).toBeFalsy();
-  expect(missFrameMatcher.test("-/")).toBeFalsy();
-  expect(missFrameMatcher.test("--")).toBeFalsy();
-  expect(missFrameMatcher.test("x")).toBeFalsy();
-  for (let i = 1; i < 10; i++) {
-    expect(missFrameMatcher.test(`${i}/`)).toBeFalsy();
-  }
-  for (let i = 1; i < 9; i++) {
-    expect(missFrameMatcher.test(`${i}${9 - i}`)).toBeFalsy();
-  }
+describe("Match missed frames", () => {
+  it("Correct", () => {
+    expect(missFrameMatcher.test("-")).toBeTruthy();
+  });
+
+  it("Don't match spares, strikes, open frames and tool ong frames", () => {
+    expect(missFrameMatcher.test("//")).toBeFalsy();
+    expect(missFrameMatcher.test("-/")).toBeFalsy();
+    expect(missFrameMatcher.test("--")).toBeFalsy();
+    expect(missFrameMatcher.test("x")).toBeFalsy();
+    for (let i = 1; i < 10; i++) {
+      expect(missFrameMatcher.test(`${i}/`)).toBeFalsy();
+    }
+    for (let i = 1; i < 9; i++) {
+      expect(missFrameMatcher.test(`${i}${9 - i}`)).toBeFalsy();
+    }
+  });
 });

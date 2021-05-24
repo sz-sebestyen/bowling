dcf07b53cbee9fee1aa54c24e9c33143f2d2be6e
Sebestyen Szabo <szabosebestyen.mail@gmail.com>
2021-05-24 14:18:05 +0200

    Add some tests for bonus ball parsing

--- BowlingGame.test.js
+++ BowlingGame.test.js
@@ -14,3 +14,25 @@ describe("Parse frames", () => {
     expect(() => BowlingGame.parseFrames("x x x x x x")).toThrow(RangeError);
   });
 });
+
+describe("Parse bonusballs", () => {
+  it("Last frame is open", () => {
+    expect(
+      BowlingGame.parseBonusBalls("x x x x x x x x x 12").length
+    ).toBeFalsy();
+  });
+
+  it("Last frame is spare", () => {
+    expect(
+      BowlingGame.parseBonusBalls("x x x x x x x x x 1/ x")[0] === "x"
+    ).toBeTruthy();
+  });
+
+  it("Last frame is strike", () => {
+    expect(
+      BowlingGame.parseBonusBalls("x x x x x x x x x x 2 3").join(" ") === "2 3"
+    ).toBeTruthy();
+  });
+
+  // TODO: write tests for errors
+});

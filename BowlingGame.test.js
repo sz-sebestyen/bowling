5e38236a192b073eda198d7a6c752d01e0a84b5e
Sebestyen Szabo <szabosebestyen.mail@gmail.com>
2021-05-24 13:42:09 +0200

    Add test for parseFrames

--- BowlingGame.test.js
+++ BowlingGame.test.js
@@ -1,5 +1,16 @@
 const BowlingGame = require("./BowlingGame");
 
-it("Nothing for  now", () => {
-  expect(true).toBeTruthy();
+// TODO: Write tests for parseFrames and parseBonusBalls
+
+describe("Parse frames", () => {
+  it("Correct", () => {
+    expect(
+      BowlingGame.parseFrames("x x x x x x x x x x x x").join(" ") ===
+        "x x x x x x x x x x"
+    ).toBeTruthy();
+  });
+
+  it("Too few frames", () => {
+    expect(BowlingGame.parseFrames("x x x x x x")).toThrow(RangeError);
+  });
 });

b0398de47777f28a2e7dea80f191eaba7aeea727
Sebestyen Szabo <szabosebestyen.mail@gmail.com>
2021-05-25 11:21:43 +0200

    Create frames from parsed frames

--- BowlingGame.js
+++ BowlingGame.js
@@ -29,7 +29,36 @@ module.exports = class BowlingGame {
 
   static createBalls(parseFrames, parsedBonusBalls) {}
 
-  static createFrames(parsedFrames) {}
+  // TODO: write tests
+  static createFrames(parsedFrames) {
+    const frames = [];
+    let numberOfBalls = 0;
+
+    parsedFrames.forEach((frame, frameIndex) => {
+      const isOpenFrame = openFrameMatcher.test(frame);
+      const isSpare = spareFrameMatcher.test(frame);
+      const isStrike = strikeFrameMatcher.test(frame);
+      const isDoubleMiss = missFrameMatcher.test(frame);
+
+      if (isOpenFrame) {
+        frames.push(
+          new OpenFrame(frameIndex, [numberOfBalls++, numberOfBalls++])
+        );
+      } else if (isSpare) {
+        frames.push(new Spare(frameIndex, [numberOfBalls++, numberOfBalls++]));
+      } else if (isStrike) {
+        frames.push(new Strike(frameIndex, [numberOfBalls++]));
+      } else if (isDoubleMiss) {
+        frames.push(
+          new OpenFrame(frameIndex, [numberOfBalls++, numberOfBalls++])
+        );
+      } else {
+        throw Error("Could not identify a frame!");
+      }
+    });
+
+    return frames;
+  }
 
   static parseFrames(gameString) {
     const isTooLong = (str) => str.length > MAX_GAMESTRING_LENGTH;

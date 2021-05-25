4b1af62f4d2095518e3d7b83615a4c7939bfa721
Sebestyen Szabo <szabosebestyen.mail@gmail.com>
2021-05-25 12:04:34 +0200

    Create balls

--- BowlingGame.js
+++ BowlingGame.js
@@ -27,7 +27,41 @@ module.exports = class BowlingGame {
     return this.score;
   }
 
-  static createBalls(parseFrames, parsedBonusBalls) {}
+  static createBalls(parsedFrames, parsedBonusBalls) {
+    const balls = [];
+
+    parsedFrames.forEach((frame) => {
+      const isOpenFrame = openFrameMatcher.test(frame);
+      const isSpare = spareFrameMatcher.test(frame);
+      const isStrike = strikeFrameMatcher.test(frame);
+      const isDoubleMiss = missFrameMatcher.test(frame);
+
+      const parseBall = (char) => parseInt(char.replace("-", "0"));
+
+      if (isOpenFrame) {
+        for (const ball of frame) {
+          balls.push(new Ball(parseBall(ball), balls.length));
+        }
+      } else if (isSpare) {
+        const firstBallScore = parseBall(frame[0]);
+        balls.push(new Ball(firstBallScore, balls.length));
+        balls.push(new Ball(10 - firstBallScore, balls.length));
+      } else if (isStrike) {
+        balls.push(new Ball(10, balls.length));
+      } else if (isDoubleMiss) {
+        balls.push(new Ball(0, balls.length));
+        balls.push(new Ball(0, balls.length));
+      } else {
+        throw Error("Could not identify a frame!");
+      }
+    });
+
+    parsedBonusBalls.forEach((bonusBall) => {
+      balls.push(new Ball(parseBall(bonusBall), balls.length));
+    });
+
+    return balls;
+  }
 
   // TODO: write tests
   static createFrames(parsedFrames) {

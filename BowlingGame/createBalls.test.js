eeb0796636164b6618ea2ea03f645a270b5bc261
Sebestyen Szabo <szabosebestyen.mail@gmail.com>
2021-05-26 09:36:21 +0200

    Add some tests for createBalls

--- BowlingGame/createBalls.test.js
+++ BowlingGame/createBalls.test.js
@@ -3,3 +3,23 @@ const { createBalls } = require("./createBalls");
 it("placeholder", () => {
   expect(true).toBeTruthy();
 });
+
+const parsedFrames = ["x", "-", "13", "44", "x", "1/", "-/", "x", "1-", "x"];
+
+const parsedBonusBalls = ["-", "2"];
+
+describe("Create the correct number of balls", () => {
+  it("No bonus balls", () => {
+    expect(createBalls(parsedFrames, []).length === 16).toBeTruthy();
+  });
+
+  it("One bonus ball", () => {
+    expect(createBalls(parsedFrames, ["x"]).length === 17).toBeTruthy();
+  });
+
+  it("Two bonus balls", () => {
+    expect(
+      createBalls(parsedFrames, parsedBonusBalls).length === 18
+    ).toBeTruthy();
+  });
+});

b8b4531864eaab068b737543376365a7eba3240b
Sebestyen Szabo <szabosebestyen.mail@gmail.com>
2021-05-26 11:29:43 +0200

    Comment out console logs

--- BowlingGame/BowlingGame.test.js
+++ BowlingGame/BowlingGame.test.js
@@ -2,16 +2,16 @@ const { BowlingGame } = require("./BowlingGame");
 
 it("all strike", () => {
   const game = new BowlingGame("x x x x x x x x x x x x");
-  console.log("balls: ", game.balls);
-  console.log("frames: ", game.frames);
+  // console.log("balls: ", game.balls);
+  // console.log("frames: ", game.frames);
 
   const score = game.getScore();
-  console.log("calculated: ", score);
+  // console.log("calculated: ", score);
   expect(score === 300).toBeTruthy();
 });
 
 it("ends on spare", () => {
   const score = new BowlingGame("x 35 9/ -7 -/ x 12 51 - 4/ x").getScore();
-  console.log("calculated: ", score);
+  // console.log("calculated: ", score);
   expect(score === 105).toBeTruthy();
 });

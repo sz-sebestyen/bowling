689748d660d4635ae6f205c33410b9fa9ec39c07
Sebestyen Szabo <szabosebestyen.mail@gmail.com>
2021-05-26 09:55:51 +0200

    Write tests for createFrames

--- BowlingGame/createFrames.test.js
+++ BowlingGame/createFrames.test.js
@@ -1,5 +1,31 @@
 const { createFrames } = require("./createFrames");
 
-it("placeholder", () => {
-  expect(true).toBeTruthy();
+const { OpenFrame, Spare, Strike } = require("./Frame");
+jest.mock("./Frame");
+
+const parsedFrames = ["x", "-", "13", "44", "x", "1/", "-/", "x", "1-", "x"];
+
+describe("Create the correct number of frames", () => {
+  it("Correct number of frames", () => {
+    expect(createFrames(parsedFrames).length === 10).toBeTruthy();
+  });
+
+  beforeEach(() => {
+    OpenFrame.mockClear();
+    Spare.mockClear();
+    Strike.mockClear();
+    createFrames(parsedFrames);
+  });
+
+  it("Correct number of openframes", () => {
+    expect(OpenFrame).toHaveBeenCalledTimes(4);
+  });
+
+  it("Correct number of spares", () => {
+    expect(Spare).toHaveBeenCalledTimes(2);
+  });
+
+  it("Correct number of strikes", () => {
+    expect(Strike).toHaveBeenCalledTimes(4);
+  });
 });

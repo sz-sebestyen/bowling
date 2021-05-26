59e45a24ffbd60c4c329aae456ec56fcf9000a30
Sebestyen Szabo <szabosebestyen.mail@gmail.com>
2021-05-26 11:18:06 +0200

    Add test to BowligGame

--- BowlingGame/BowlingGame.test.js
+++ BowlingGame/BowlingGame.test.js
@@ -1,5 +1,13 @@
 const { BowlingGame } = require("./BowlingGame");
 
-it("placeholder", () => {
-  expect(true).toBeTruthy();
+it("all strike", () => {
+  const score = new BowlingGame("x x x x x x x x x x x x").getScore();
+  console.log("calculated: ", score);
+  expect(score === 300).toBeTruthy();
+});
+
+it("ends on spare", () => {
+  const score = new BowlingGame("x 35 9/ -7 -/ x 12 51 - 4/ x").getScore();
+  console.log("calculated: ", score);
+  expect(score === 105).toBeTruthy();
 });

cf9fbe4b708d8a385fac37dea668fe740cb914c0
Sebestyen Szabo <szabosebestyen.mail@gmail.com>
2021-05-27 11:38:02 +0200

    Document Ball class

--- BowlingGame/Ball.js
+++ BowlingGame/Ball.js
@@ -1,4 +1,18 @@
+/**
+ * Ball module.
+ * @module Ball
+ */
+
+/**
+ * Creates a Ball with a score and index.
+ * @class
+ * @classdesc Represents a ball in the bowling game.
+ */
 module.exports = class Ball {
+  /**
+   * @param {number} numberOfPinsKnocked The number of pins knocked down.
+   * @param {number} index The index of the ball.
+   */
   constructor(numberOfPinsKnocked, index) {
     this.score = numberOfPinsKnocked;
     this.index = index;

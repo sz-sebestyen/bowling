1ed1e688fc00c9153e0ef7084c4b4549c8daa830
Sebestyen Szabo <szabosebestyen.mail@gmail.com>
2021-05-27 11:38:22 +0200

    Document calculateScore

--- calculateScore.js
+++ calculateScore.js
@@ -1,5 +1,15 @@
+/**
+ * calculateScore module.
+ * @module calculateScore
+ */
+
 const BowlingGame = require("./BowlingGame");
 
+/**
+ * Calculates the score of a single bowling game.
+ * @param {string} gameString The bowling game represented as a string.
+ * @returns {number} The score of the bowling game.
+ */
 module.exports = function calculateScore(gameString) {
   return new BowlingGame(gameString).getScore();
 };
